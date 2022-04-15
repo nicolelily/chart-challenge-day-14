f482561e16fd52657983b7745090de42b8091185
Nicole Mark <nicolemark@gmail.com>
2022-04-14 23:34:27 -0400

    fixed spelling error

--- chart_challenge.py
+++ chart_challenge.py
@@ -21,9 +21,6 @@ st.markdown(
 
 st.markdown(">This is my favorite part about analytics: Taking boring flat data and bringing it to life through visualization \n\n—John Tukey")
 
-
-st.plotly_chart(
-
 fig = px.scatter_3d(df, x="Number of charts used in production (last 6 months)", y='Number of data visualization tools', z='Number of channels used for sharing visualizations',
               color="Number of charts used in production (last 6 months)", labels={
                      "Number of data visualization tools": "Number of tools",
@@ -31,7 +28,7 @@ fig = px.scatter_3d(df, x="Number of charts used in production (last 6 months)",
                      "Number of channels used for sharing visualizations": "Number of channels"}, color_continuous_scale = "darkmint",
                    title="The number of tools, chart types, and channels for sharing used by data visualization practitioners")
 
-)
+st.write(fig)
 
 st.markdown("Check out my [new website](https://www.nicoledesignsdata.com")
 
