78ec24e63fca61c0ba2a3eb5585194f0c4133572
Nicole Mark <nicolemark@gmail.com>
2022-04-14 23:13:13 -0400

    third time's the charm

--- chart_challenge.py
+++ chart_challenge.py
@@ -16,8 +16,7 @@ df = get_data()
 
 st.title("📊 #30DayChartChallenge 2022 Day 14: 3 dimensional 📈")
 st.markdown(
-    "I could have shared a static image only, but then y'all wouldn't have seen the super cool interactivity of Plotly Express's 3-D scatterplot. You can zoom in, out, pan around the view, and see each mark from all angles. Hover to view tooltips. \n
-    This data is from the '2021' Data Visualization Society State of the Industry Survey.")
+    "I could have shared a static image only, but then y'all wouldn't have seen the super cool interactivity of Plotly Express's 3-D scatterplot. You can zoom in, out, pan around the view, and see each mark from all angles. Hover to view tooltips. This data is from the '2021' Data Visualization Society State of the Industry Survey.")
 
 st.markdown(">This is my favorite part about analytics: Taking boring flat data and bringing it to life through visualization"\n\n—John Tukey")
 
@@ -34,7 +33,7 @@ fig = px.scatter_3d(df, x="Number of charts used in production (last 6 months)",
 fig.show()
 )
 
-st.markdown("Check out my [new website](https://www.nicoledesignsdata.com))
+st.markdown("Check out my [new website](https://www.nicoledesignsdata.com"))
 
 st.write("You know you want to click that button 👇🏼")
 btn = st.button("Celebrate!")
